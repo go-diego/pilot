d5a4cf1162753fb159425069290b394403cf1b07
Diego Bernal <diego91bernal@gmail.com>
2018-09-13 15:19:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ import Service from "./service.js";
 const service = new Service();
 
 service.getApiKey().then(response => console.log(`YOU APIKEY IS ${response}`));
+service.getSuggestions("Lorem Ipsum").then(response => console.log(`Should return "Lorem Ipsum": ${response}`));
